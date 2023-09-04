1ae05fef9e22930158bdafb23a8103ce600466c8
carlosau <carlosaugsz@hotmail.com>
2023-09-04 10:56:20 -0300

    testing useState before add redux

--- app/components/Demo.js
+++ app/components/Demo.js
@@ -1,23 +1,34 @@
 'use client'
 import { BsLink } from 'react-icons/bs'; 
 import { BiSolidSend } from 'react-icons/bi';
+import { useState } from 'react';
 
 export default function Demo() {
+    const [article, setArticle] = useState({
+        url: '',
+        summary: '',
+    })
+
+    const handleSubmit = async (e) => {
+        alert('submitted')
+    }
+
     return (
         <section className="mx-auto m-10 w-full max-w-xl">
             {/* Search bar */}
             <div className='flex flex-col w-full gap-2'>
                 <form
                     className='relative flex justify-center items-center'
-                    onSubmit ={() => {}}
+                    onSubmit ={handleSubmit}
                     >
                     <BsLink size={25} className='absolute left-0 my-2 ml-3'/>
                     <input
                         type='url'
                         placeholder='Cole o link aqui'
-                        value=""
-                        onChange={() => {}}
+                        value={article.url}
+                        onChange={(e) => setArticle({ ...article, url: e.target.value })}
                         className='url_input peer'
+                        required
                     />
                     <button
                         type='submit'
