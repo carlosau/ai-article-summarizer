a15c5e1ded1e4298dae3e8f8bd382e7e3352c92b
carlosau <carlosaugsz@hotmail.com>
2023-09-04 10:36:51 -0300

    finished frontend part

--- app/components/Demo.js
+++ app/components/Demo.js
@@ -1,7 +1,39 @@
+'use client'
+import { BsLink } from 'react-icons/bs'; 
+import { BiSolidSend } from 'react-icons/bi';
+
 export default function Demo() {
     return (
-        <div className="m-10 text-center">
-            <h1>Demo</h1>
-        </div>
+        <section className="mx-auto m-10 w-full max-w-xl">
+            {/* Search bar */}
+            <div className='flex flex-col w-full gap-2'>
+                <form
+                    className='relative flex justify-center items-center'
+                    onSubmit ={() => {}}
+                    >
+                    <BsLink size={25} className='absolute left-0 my-2 ml-3'/>
+                    <input
+                        type='url'
+                        placeholder='Cole o link aqui'
+                        value=""
+                        onChange={() => {}}
+                        className='url_input peer'
+                    />
+                    <button
+                        type='submit'
+                        className='submit_btn
+                        peer-focus:border-gray-700
+                        peer-focus:text-gray-700
+                        '
+                    >
+                        <BiSolidSend />
+                    </button>
+                </form>
+
+                {/* Browse URL History */}
+           </div>
+            
+            {/* Result */}
+        </section>
     )
 }
--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,19 +1,24 @@
 import Image from "next/image";
+import { GrShareOption } from "react-icons/gr";
 
 export default function Hero() {
   return (
     <div>
-    <header className="w-full flex flex justify-center items-center flex-col">
-      <nav className="flex w-full justify-between items-center mx-20">
-        <Image src="img/logo.svg" alt="logo" width={200} height={200} />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Contact
-        </button>
-      </nav>
-    </header>
+      <header className="w-full flex flex justify-center items-center flex-col">
+        <nav className="flex w-full justify-between items-center mx-20">
+          <Image src="img/logo.svg" alt="logo" width={200} height={200} />
+          <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
+            <GrShareOption color="white"/>
+          </button>
+        </nav>
+      </header>
 
-    <h1 className="text-3xl font-bold text-center mt-20">Resumo de link com ChatGPT-4,</h1>
-    <p className="text-center font-bold text-3xl bg-gradient-to-r from-rose-500 via-sky-600 to-yellow-500 bg-clip-text text-transparent">Cole o link abaixo e obtenha o seu resumo!</p>
+      <h1 className="text-3xl font-bold text-center mt-20">
+        Resumo de link com ChatGPT-4,
+      </h1>
+      <p className="text-center font-bold text-3xl bg-gradient-to-r from-rose-500 via-sky-600 to-yellow-500 bg-clip-text text-transparent">
+        Cole o link abaixo e obtenha o seu resumo!
+      </p>
     </div>
   );
 }
